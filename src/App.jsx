9f4f90ce1047361e3a80003a868d9d407eea5de0
Arno luyckx <A.luyckx@protonmail.com>
2023-03-21 00:43:26 +0100

    Fully functional, and minimal styling. Attempting Charts.js next

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,97 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoneyBillTransfer } from '@fortawesome/free-solid-svg-icons'
+import { faMoneyBillTransfer, faSterlingSign, faEuroSign, faYenSign, faDollarSign, faUser, faBuildingColumns } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 
 
 
+
 function App() {
-  // ! useState is a hook that allows us to use state in functional components.
+  // ! Hooks are used to manage state in functional components in modern React.
   const [rate, setRate] = useState(0)
-  
-  // ! Will have to set the state based on the select tag.
   const [baseCurrency, setBaseCurrency] = useState('USD')
   const [quoteCurrency, setQuoteCurrency] = useState('EUR')
-  const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState(1)
+  const [converted, setConverted] = useState(0)
 
-  // ! API used, free with 500 calls a day.
-  // ? https://www.alphavantage.co/documentation/#fx
   const apiKey = 'S765H6N4CCK2VPCH'
-  const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=${apiKey}`
-  const monthly = `https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=USD&to_symbol=EUR&apikey=${apiKey}`
-  let currentRate = 0;
-  // ? Fetching data from API, setting the rate to the currentRate. State needs to be set to update the DOM.
-  // ! State is immutable, so we need to use the setRate function to update the state.
-  const apiCall = () => {
+  const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${baseCurrency}&to_currency=${quoteCurrency}&apikey=${apiKey}`;
+  //const monthly = `https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=USD&to_symbol=EUR&apikey=${apiKey}`
+
+  useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
         console.log(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
-        currentRate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+        const currentRate = parseFloat(data['Realtime Currency Exchange Rate']['5. Exchange Rate']).toFixed(3);
+        
         setRate(currentRate);
+        setConverted(currentRate * amount)
       }) 
-  } 
+  }, [baseCurrency, quoteCurrency]);
 
-  // ! This function will be called when the input is changed.
-  const convert = (e) => {
-    //e.preventDefault();
-    // ? Always place the value in a variable, and then set the state.
+  const convert = (e) => { 
     const amount = e.target.value;
+    const convertedAmount = (amount * rate).toFixed(2);
     setAmount(amount)
-    console.log(amount)
+    setConverted(convertedAmount)
   }
 
   return (
     <div className="App">
-
-      {/* //TODO abstract some things into a component later. */}
-      {/* Header.... */}
       <h1>Altcademy React Currency</h1>
-      <h2>USD/EUR {rate}</h2>
+      <h2>{baseCurrency}/{quoteCurrency} {rate}</h2>
       
     <>
       <div className="card">
-        <select name='base-currency' id='base-currency'>
+        <select name='base-currency' id='base-currency' value={baseCurrency} onChange={(e) => setBaseCurrency(e.target.value)}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
-          <option value="GBP">GBP</option>
-          <option value="JPY">JPY</option>
+          <option value="GBP">GBP</option> 
+          <option value="JPY">JPY</option> 
           <option value="BTC">BTC</option>
-          <option value="AU">AU</option>
-          <option value="AG">AG</option>
         </select>
-        <FontAwesomeIcon icon={faMoneyBillTransfer} />
-        <select name="quote-currency" id="quote-currency">
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="GBP">GBP</option>
-          <option value="JPY">JPY</option>
-          <option value="BTC">BTC</option>
-          <option value="AU">AU</option>
-          <option value="AG">AG</option>
-        </select>
-      </div>
-      <div>
-        <input type="number" value={amount} onChange={convert}/>
-        <button onClick={apiCall}>Fetch</button>
-      </div>
-        
-        
 
+        <FontAwesomeIcon icon={faUser} />
+        <FontAwesomeIcon icon={faMoneyBillTransfer} className="exchange-icon" />
+        <FontAwesomeIcon icon={faBuildingColumns} />
+
+        <select name="quote-currency" id="quote-currency" value={quoteCurrency} onChange={(e) => setQuoteCurrency(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+          <option value="JPY">JPY</option>
+          <option value="BTC">BTC</option> 
+        </select>
+      </div>
+      <div className="result">
+        <p>
+          <span className="currency">{quoteCurrency} </span>
+          <span className="amount">{converted}</span>
+        </p>
+      </div>
+      <div className='input-wrapper'>
+        <input type="number" value={amount} onChange={convert}/>
+      </div>
     </>
-      {/* Selection/ main content */}
-      {/* Side bar */}
-      {/* Footer */}
     </div>
   )
 }
 
 export default App
 
-// todo: Side bar for current rates, and a graph of the last 30 days.
-// todo: Add a search bar to search for different currencies?
-// todo: Add a dropdown to select the currency you want to convert to.
+/**
+ * ! api used: https://www.alphavantage.co/documentation/
+ * 
+ * todo: Add a dropdown to select the currency you want to convert to. [DONE]
+ * todo: use Chart.js to display a graph of the exchange rate over the last 30 days. [PENDING]
+ * todo: make the dollar bill clickable to swap the currencies. [PENDING]
+ * 
+ * ? docs for React hooks: https://reactjs.org/docs/hooks-intro.html
+ * ? docs for React icons: https://fontawesome.com/how-to-use/on-the-web/using-with/react
+ * ? docs for React useEffect: https://reactjs.org/docs/hooks-effect.html
+ * ? docs for React useState: https://reactjs.org/docs/hooks-state.html
+ * 
+ * TODO: Ask david about how to safeguard the api key, when I can't use .env like I would in a NodeJs app.
+ */
