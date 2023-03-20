bc9de74903a2d56ade533a7657675d663af4ac31
Arno luyckx <A.luyckx@protonmail.com>
2023-03-20 19:40:48 +0100

    Added selects for base and quote currency, working on the states with hooks...

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoneyBillTransfer } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 
-// ! API used, free with 500 calls a day.
-// ? https://www.alphavantage.co/documentation/#fx
-const apiKey = 'S765H6N4CCK2VPCH'
-const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=${apiKey}`
-let currentRate = 0;
 
-
-//const symbols = ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'HKD', 'NZD', 'SEK', 'SGD', 'ZAR']
 
 function App() {
   // ! useState is a hook that allows us to use state in functional components.
-  const [count, setCount] = useState(0)
   const [rate, setRate] = useState(0)
   
+  // ! Will have to set the state based on the select tag.
+  const [baseCurrency, setBaseCurrency] = useState('USD')
+  const [quoteCurrency, setQuoteCurrency] = useState('EUR')
+  const [amount, setAmount] = useState(0)
+
+  // ! API used, free with 500 calls a day.
+  // ? https://www.alphavantage.co/documentation/#fx
+  const apiKey = 'S765H6N4CCK2VPCH'
+  const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=${apiKey}`
+  const monthly = `https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=USD&to_symbol=EUR&apikey=${apiKey}`
+  let currentRate = 0;
   // ? Fetching data from API, setting the rate to the currentRate. State needs to be set to update the DOM.
   // ! State is immutable, so we need to use the setRate function to update the state.
   const apiCall = () => {
@@ -28,27 +33,62 @@ function App() {
       }) 
   } 
 
+  // ! This function will be called when the input is changed.
+  const convert = (e) => {
+    //e.preventDefault();
+    // ? Always place the value in a variable, and then set the state.
+    const amount = e.target.value;
+    setAmount(amount)
+    console.log(amount)
+  }
+
   return (
     <div className="App">
 
-      {/* TODO abstract some things into a component later. */}
+      {/* //TODO abstract some things into a component later. */}
+      {/* Header.... */}
       <h1>Altcademy React Currency</h1>
+      <h2>USD/EUR {rate}</h2>
+      
+    <>
       <div className="card">
-
-        {/* ! Keep for syntax reference. for now */}
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={apiCall}>Fetch</button>
-        <div>
-          <p>USD {rate}</p>
-          <p>EUR</p>
-        </div>
-
+        <select name='base-currency' id='base-currency'>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+          <option value="JPY">JPY</option>
+          <option value="BTC">BTC</option>
+          <option value="AU">AU</option>
+          <option value="AG">AG</option>
+        </select>
+        <FontAwesomeIcon icon={faMoneyBillTransfer} />
+        <select name="quote-currency" id="quote-currency">
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+          <option value="JPY">JPY</option>
+          <option value="BTC">BTC</option>
+          <option value="AU">AU</option>
+          <option value="AG">AG</option>
+        </select>
       </div>
+      <div>
+        <input type="number" value={amount} onChange={convert}/>
+        <button onClick={apiCall}>Fetch</button>
+      </div>
+        
+        
 
+    </>
+      {/* Selection/ main content */}
+      {/* Side bar */}
+      {/* Footer */}
     </div>
   )
 }
 
 export default App
+
+// todo: Side bar for current rates, and a graph of the last 30 days.
+// todo: Add a search bar to search for different currencies?
+// todo: Add a dropdown to select the currency you want to convert to.
